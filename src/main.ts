0a9f6625972e61e0858a37c95731a82374fd13e9
Maria Eduarda <mariaeduarda-souza@hotmail.com.br>
2022-03-21 16:58:51 -0300

    feat: add filters for http exeption

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as momentTimezone from 'moment-timezone';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  Date.prototype.toJSON = function (): any {
+    return momentTimezone(this)
+      .tz('America/Sao_Paulo')
+      .format('YYYY-MM-DD HH:mm:ss.SSS');
+  };
+
   await app.listen(3000);
 }
 bootstrap();
